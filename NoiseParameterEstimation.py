3d0a390ea698f27a48f0da59991a2bfadf79430a
Luis C. Wohlers <59035138+LuisWohlers@users.noreply.github.com>
2021-10-10 19:28:46 +0200

    Add files via upload

---
+++ NoiseParameterEstimation.py
@@ -0,0 +1,132 @@
+from scipy.stats import kurtosis
+import scipy
+import numpy as np
+
+
+def im2col(A, M1, M2):
+    # according to https://stackoverflow.com/questions/30109068/implement-matlabs-im2col-sliding-in-python
+    A = A.T
+    m, n = A.shape
+    s0, s1 = A.strides
+    nrows = m - M1 + 1
+    ncols = n - M2 + 1
+    shp = M1, M2, nrows, ncols
+    strd = s0, s1, s0, s1
+
+    out_view = np.lib.stride_tricks.as_strided(A, shape=shp, strides=strd)
+    return out_view.reshape(M1 * M2, -1)
+
+
+def get_valid_block_index(image, M1, M2):
+    block = im2col(image.T, M1, M2)
+    invalid_grayvalue = []
+    for j in range(0, block.shape[1]):
+        min_gv = min(block[:, j])
+        max_gv = max(block[:, j])
+        if min_gv == max_gv and not min_gv in invalid_grayvalue:
+            invalid_grayvalue.append(min_gv)
+
+    valid_block_index = np.zeros((block.shape[1], 1))
+    valid_block_index_count = 0
+    for j in range(0, block.shape[1]):
+        block_ok = True
+        for i in range(0, block.shape[0]):
+            if block[i, j] <= 0 or block[
+                i, j] in invalid_grayvalue or block[i, j] >= 255:
+                block_ok = False
+        if block_ok:
+            valid_block_index_count = valid_block_index_count + 1
+            valid_block_index[valid_block_index_count - 1] = j
+    return valid_block_index[0:valid_block_index_count].astype(int)
+
+
+def get_blocks(image, phi, row_parity, valid_block_index, M1, M2):
+    block = im2col(VST(image.T, phi), M1, M2)
+    block = block[int(row_parity) - 1::2, valid_block_index]
+    return np.squeeze(block).T
+
+
+def sort_blocks(image, phi, valid_block_index, M1, M2):
+    block = get_blocks(image, phi, 2, valid_block_index, M1, M2)
+    block = pca_svd_score(block)
+    block = np.square(block)
+    T = np.column_stack((valid_block_index, np.sum(block[:, 3::], axis=1)))
+    T = np.array(T)
+    T = T[np.argsort(T[:, 1])]
+    return T[:, 0]
+
+
+def VST(t, phi):
+    a = np.cos(phi)
+    b = np.sin(phi)
+    if a > 2.220446049250313e-16:  # np.finfo(float).eps
+        return (2 / a) * np.sqrt(a * t + b)
+    else:
+        return t / np.sqrt(b)
+
+
+def compute_std(image, phi, tau, block_count, M1, M2):
+    block = get_blocks(image, phi, 1, tau[0:block_count], M1, M2)
+    latent = pca_svd_latent(block)
+    return np.sqrt(latent[-1])
+
+
+def optimize(func, a, b, accuracy, image, tau, block_count, M1, M2):
+    opt_x = scipy.optimize.fminbound(func, a, b, args=(image, tau, block_count, M1, M2), xtol=accuracy, maxfun=500,
+                                     full_output=0, disp=0)
+    return opt_x
+
+
+def pca_svd_score(x):
+    x -= np.mean(x, axis=0)
+    U, S, Vt = scipy.linalg.svd(x, full_matrices=False)
+    return U * S
+
+
+def pca_svd_latent(x):
+    x -= np.mean(x, axis=0)
+    U, S, V = scipy.linalg.svd(x, full_matrices=False)
+    return (S ** 2) / (x.shape[0] - 1)
+
+
+def compute_kurtosis(phi, image, tau, block_count, M1, M2):
+    block = get_blocks(image, phi, 1, tau[0:block_count], M1, M2)
+    score = pca_svd_score(block)
+    G = (kurtosis(score[:, -1], fisher=False) - 3) * np.sqrt(block_count / 24)
+    return G
+
+
+def estimate_noise_parameters(image, blocksize):
+    M1 = blocksize
+    M2 = blocksize
+
+    valid_block_index = get_valid_block_index(image, M1, M2)
+
+    phi = [0.0]
+    sigma = [0.0]
+    while (len(phi) < 20) and (len(phi) < 3 or np.min(np.abs(phi[1:-1] - phi[-1])) > 0.001):
+
+        a = np.square(sigma[-1]) * np.cos(phi[-1])
+        b = np.square(sigma[-1]) * np.sin(phi[-1])
+        tau = sort_blocks(image, phi[-1], valid_block_index, M1, M2).astype(int)
+        block_count = 5000
+        curr_phi = 0
+        curr_sigma = 0
+
+        while block_count <= len(tau):
+
+            opt_phi = optimize(compute_kurtosis, 0, np.pi / 2 - 0.001, 0.001, image, tau, block_count, M1,
+                               M2)
+            opt_kurtosis = compute_kurtosis(opt_phi, image, tau, block_count, M1, M2)
+            if opt_kurtosis < 3 or curr_phi == 0:
+                phi_converged = np.abs(opt_phi - curr_phi) < 0.0001
+                curr_phi = opt_phi
+                curr_sigma = compute_std(image, opt_phi, tau, block_count, M1, M2)
+                if phi_converged:
+                    break
+            else:
+                break
+            block_count = block_count + 25000
+        phi.append(curr_phi)
+        sigma.append(curr_sigma)
+    return a, b
---
+++ RenoisePCA.py
@@ -0,0 +1,103 @@
+import scipy
+import numpy as np
+from math import sqrt
+import NoiseParameterEstimation as est
+import time
+
+
+def im2col(A, M1, M2):
+    # according to https://stackoverflow.com/questions/30109068/implement-matlabs-im2col-sliding-in-python
+    A = A.T
+    m, n = A.shape
+    s0, s1 = A.strides
+    nrows = m - M1 + 1
+    ncols = n - M2 + 1
+    shp = M1, M2, nrows, ncols
+    strd = s0, s1, s0, s1
+
+    out_view = np.lib.stride_tricks.as_strided(A, shape=shp, strides=strd)
+    return out_view.reshape(M1 * M2, -1)
+
+
+def pca_svd_latent(x):
+    x -= np.mean(x, axis=0)
+    U, S, V = scipy.linalg.svd(x, full_matrices=False)
+    return (S ** 2) / (x.shape[0] - 1)
+
+
+def VSTab(t, a, b, sigma):
+    if a == 0 and b == 0:
+        return t
+    if a > 2.220446049250313e-16:  # np.finfo(float).eps
+        return (2 * sigma / a) * np.sqrt(a * t + b)
+    else:
+        return t / np.sqrt(b)
+
+
+def VSTreverse(y, a, b, sigma, bitdepth):
+    image = (np.square((y * a) / (2 * sigma)) - b) / a
+    image[image > (pow(2, bitdepth) - 1)] = pow(2, bitdepth) - 1
+    image[image < 0] = 0
+    return image.round()
+
+
+def lambda_adjust(regionsize, blocksize):
+    lambdas = np.zeros(10000)
+    for j in range(0, 10000):
+        noisematrix = np.random.normal(0, 1, (regionsize, regionsize))
+        dataset = im2col(noisematrix, blocksize, blocksize)
+        latent = pca_svd_latent(dataset.T)
+        lambdaP = latent[-1]
+        lambdas[j] = lambdaP
+        s_lambda = (1 / np.mean(lambdas)).round(3)
+    return s_lambda
+
+
+def renoisePCA(image, regionsize, blocksize, sigma):
+    imgpadded = np.pad(image, np.floor(regionsize / 2).astype(int), "symmetric")
+    M = image.shape[0]
+    N = image.shape[1]
+    renoised = np.empty((M, N))
+    s_lambda = lambda_adjust(regionsize, blocksize)
+    for i in range(0, M):
+        for j in range(0, N):
+            latent = pca_svd_latent(im2col(imgpadded[i:i + regionsize, j:j + regionsize], blocksize, blocksize).T)
+            lambda_p_adjusted = (latent[-1] * s_lambda)
+            if lambda_p_adjusted < np.square(sigma):
+                vartoadd = abs(np.square(sigma) - lambda_p_adjusted)
+                renoised[i, j] = image[i, j] + np.random.normal(0, sqrt(vartoadd))
+            else:
+                renoised[i, j] = image[i, j]
+    return renoised
+
+
+def reconstructNoise(originalimage, degradedimage, bitdepth, analyse_blocksize, renoise_regionsize, renoise_blocksize,
+                     sigma):
+    starttime = time.time()
+    a_original, b_original = est.estimate_noise_parameters(originalimage, analyse_blocksize)
+    endtime = time.time() - starttime
+    print("Analysis of a and b params of original image finished in (seconds):")
+    print(endtime)
+    print("a param of original image:")
+    print(a_original)
+    print("b param of original image:")
+    print(b_original)
+
+    VSTdegradedimage = VSTab(degradedimage, a_original, b_original, sigma).astype(float)
+    starttime = time.time()
+    renoisedVST = renoisePCA(VSTdegradedimage, renoise_regionsize, renoise_blocksize, sigma)
+    endtime = time.time() - starttime
+    print("Renoise time:")
+    print(endtime)
+    finalimage = VSTreverse(renoisedVST, a_original, b_original, sigma, bitdepth)
+    starttime = time.time()
+    a_final, b_final = est.estimate_noise_parameters(finalimage, analyse_blocksize)
+    endtime = time.time() - starttime
+    print("Analysis of a and b params of final image finished in (seconds):")
+    print(endtime)
+    print("a param of final image:")
+    print(a_final)
+    print("b param of final image:")
+    print(b_final)
+
+    return finalimage, a_original, b_original, a_final, b_final
