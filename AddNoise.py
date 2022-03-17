f5f565d55528699d768706913e0bab4974a64059
LuisWohlers <luis-wohlers@gmx.net>
2022-03-17 14:47:14 +0100

    update

--- AddNoise.py
+++ AddNoise.py
@@ -1,12 +1,15 @@
+"""add Poisson-Gaussian noise to image"""
 import numpy as np
 
-def AddPoissonGaussianNoise_RGB(image,a,b,bits):
-    if a == 0:
-        y = image
+def add_PoissonGaussianNoise_RGB(image,param_a,param_b,bits):
+    """add Poisson-Gaussian noise with parameters a and b to input image"""
+    if param_a == 0:
+        output_y = image
     else:
-        y = np.random.poisson(image/a)*a
-    y = y + np.sqrt(b)*np.random.randn(y.shape[0],y.shape[1],y.shape[2])
-    y[y>pow(2,bits)-1] = pow(2,bits)-1
-    y[y<0] = 0
-    return y.round(0)  
+        output_y = np.random.poisson(image/param_a)*param_a
+    output_y = output_y + np.sqrt(param_b)*np.random.randn(output_y.shape[0],
+                                    output_y.shape[1],output_y.shape[2])
+    output_y[output_y>pow(2,bits)-1] = pow(2,bits)-1
+    output_y[output_y<0] = 0
+    return output_y.round(0)  
 
